ac3a4f0ff9bd13eeacc894a167fdd9e8f7632fed
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-25 19:08:29 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,17 +6,17 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-introduction("josh")
+
 
 def introduction_with_language(name,language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language("Dan","Ember.js")
+
 
 def introduction_with_language_optional(name,language = "Ruby")
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language_optional("Edwin")
+
 
 
 
